f23fcf2e7347ced95956af3f5a1afc6c9e03e445
Christian Aracena <CDAracena@gmail.com>
2018-11-23 12:23:36 -0500

    submit excercise object to excercise array

--- src/App.js
+++ src/App.js
@@ -12,20 +12,33 @@ class App extends Component {
     excercises: [],
     title: ''
   }
-    handleChange = ({ target: {name, value}}) => {
+    handleChange = (input) => {
+      this.setState({title: input.target.value})
+    }
+
+    handleCreate = e => {
+      e.preventDefault();
+
+    if (this.state.title) {
+      console.log(this.state.title)
       this.setState({
-        [name]: value
+        excercises: [...this.state.excercises, {title: this.state.title, id: Date.now()}],
+        title: ''
       })
     }
+  }
   render(){
+    const { title } = this.state
     return (
       <Paper>
       <Typography variant="display2" align='center' gutterBottom>
         Exercises
       </Typography>
-      <form>
-        <TextField name="title" label="Exercise" value={this.state.title} onChange={this.handleChange} margin='normal'/>
+      <form onSubmit={this.handleCreate}>
+        <TextField name="title" label="Exercise" value={title} onChange={this.handleChange} margin='normal'/>
+        <Button type='submit' color='primary' variant='fab' size='medium'> Create </Button>
       </form>
+
     </Paper>
     )
   }
