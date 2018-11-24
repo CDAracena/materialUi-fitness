c9eab4adc1e6020972b35fe6be42241d8d321ca3
Christian Aracena <CDAracena@gmail.com>
2018-11-23 21:05:00 -0500

    added delete functionality, set primary and secondary colors with material ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMuiTheme } from '@material-ui/core/styles'
+import red from '@material-ui/core/colors/red';
+import grey from '@material-ui/core/colors/grey'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    secondary: grey
+  }
+})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}> <App/> </MuiThemeProvider>, document.getElementById("root"))
